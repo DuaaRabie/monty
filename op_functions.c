c05c964f6bfb6c45a1ee50b62a8cab5a7452bdf5
DuaaRabie <Duaarabie11@gmail.com>
2023-11-16 11:29:55 -0800

    update code

--- op_functions.c
+++ op_functions.c
@@ -66,10 +66,10 @@ void op_pint(stack_t **stack, unsigned int line_number)
 	stack_t *temp = *stack;
 
 	if (temp != NULL)
-		printf("%d\n", temp->n);
+		fprintf(stdout, "%d\n", temp->n);
 	else
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 }
