7b17d31402ef9380f1bdc35fd01ca8551a522248
DuaaRabie <Duaarabie11@gmail.com>
2023-11-16 15:31:39 -0800

    update code

--- get_op_functions.c
+++ get_op_functions.c
@@ -9,13 +9,13 @@ void (*get_op(char *s))(stack_t **, unsigned int)
 {
 	int i = 0;
 	instruction_t ops[] = {
-		{"push", op_push},
-		{"pall", op_pall},
-		{"pint", op_pint},
-		{"pop", op_pop},
-		{"swap", op_swap},
-		{"nop", op_nop},
-		{"add", op_add},
+		{"push", &op_push},
+		{"pall", &op_pall},
+		{"pint", &op_pint},
+		{"pop", &op_pop},
+		{"swap", &op_swap},
+		{"add", &op_add},
+		{"nop", &op_nop},
 		{NULL, NULL}
 	};
 
