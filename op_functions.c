be003bf6b3c162c0d5f20551b47a27d3ccd0dc03
DuaaRabie <Duaarabie11@gmail.com>
2023-11-16 11:52:10 -0800

    update code

--- op_functions.c
+++ op_functions.c
@@ -92,7 +92,7 @@ void op_pop(stack_t **stack, unsigned int line_number)
 	}
 	else
 	{
-		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%u: can't pop an empty stack", line_number);
 		exit(EXIT_FAILURE);
 	}
 }
