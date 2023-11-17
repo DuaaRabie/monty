f0256aa045662c0acd7fd6afa198277f500da2ea
DuaaRabie <Duaarabie11@gmail.com>
2023-11-16 18:49:44 -0800

    update code

--- op_functions1.c
+++ op_functions1.c
@@ -112,7 +112,7 @@ void op_swap(stack_t **stack, unsigned int line_number)
 	}
 	else
 	{
-		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
