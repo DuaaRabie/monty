b2318c4b59ba1704bf76815c9f44c8880b79ee8e
DuaaRabie <Duaarabie11@gmail.com>
2023-11-16 18:24:23 -0800

    update code

--- monty.c
+++ monty.c
@@ -62,7 +62,7 @@ void interpret_line(char *line, unsigned int line_number)
 		free(line);
 		free(vars.topush);
 		fclose(vars.fp);
-		fprintf(stderr, "L%u: unknown instruction <opcode>\n", line_number);
+		fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
 		free(opcode);
 		exit(EXIT_FAILURE);
 	}
