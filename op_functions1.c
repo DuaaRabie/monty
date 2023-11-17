be0d6a1fc75ab290e352e222d5954f86ef443501
DuaaRabie <Duaarabie11@gmail.com>
2023-11-16 16:02:21 -0800

    update code

--- op_functions1.c
+++ op_functions1.c
@@ -50,7 +50,7 @@ void op_pall(stack_t **stack, unsigned int line_number)
 	(void)line_number;
 	while (temp != NULL)
 	{
-		printf("%d\n", temp->n);
+		fprintf(stderr, "%d\n", temp->n);
 		temp = temp->next;
 	}
 }
@@ -64,7 +64,7 @@ void op_pall(stack_t **stack, unsigned int line_number)
 void op_pint(stack_t **stack, unsigned int line_number)
 {
 	if (*stack != NULL)
-		fprintf(stdout, "%d\n", (*stack)->n);
+		fprintf(stderr, "%d\n", (*stack)->n);
 	else
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
